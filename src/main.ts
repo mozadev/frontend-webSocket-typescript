9c3aeaa568d18d0e5cd1a5d025a02b33864c4ad4
mozadev <ceosmore@gmail.com>
2024-07-14 19:40:03 -0500

    feat: Add server status indicator and client list in main.ts and socket-client.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,18 @@ import './style.css'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
    <h1>Websocket - Client</h1>
+  
    <span id="server-status">Offline</span>
+
+  <ul id="clients-ul"></ul>
+
+  <form id="message-form">
+  <input placeholder="message" id="message-input" />
+  </form>
+
+<h3>Messages</h3>
+<ul id="messages-ul"></ul>
+
   </div>
 ` 
 connecToServer();
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -14,7 +14,14 @@ export const connecToServer = () => {
 
 const addListerners = (socket: Socket) => {
 
-    const serverStatusLabel = document.querySelector('#server-status')!;
+    const clientsUl = document.querySelector<HTMLOListElement>('#clients-ul')!;
+    const messageForm =document.querySelector<HTMLFormElement>('#message-form')!;
+    const messageInput = document.querySelector<HTMLInputElement>('#message-input')!;
+    const messagesUl = document.querySelector<HTMLUListElement>('#messages-ul')!;
+    const serverStatusLabel = document.querySelector<HTMLSpanElement>('#server-status')!;
+    
+
+// TODO: #clients-ul */
 
     socket.on('connect', () => {
         // console.log('Connected to server')
@@ -25,5 +32,42 @@ const addListerners = (socket: Socket) => {
         // console.log('Disconnected from server')
         serverStatusLabel.innerHTML='disconnected'
     })
+
+    socket.on('clients-updated', (clients: string[]) => {
+        let clientsHtml = '';
+        clients.forEach(clientId => {
+            clientsHtml += `<li> ${clientId}</li>`
+        })
+        clientsUl.innerHTML = clientsHtml;
+    //   console.log({clients})  
+    })
+
+    messageForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        if (messageInput.value.trim().length <= 0) return;
+        
+        socket.emit('message-from-client', {
+            id: 'YO!!',
+            message: messageInput.value,
+        })
+
+        messageInput.value = '';
+        // console.log({ id: 'YO!!', message: messageInput.value })
+    })
+
+    socket.on('message-from-server', (payload: { fullName: string, message: string }) => {
+        const newMessage = `
+        <li>
+        <strong>${payload.fullName}</strong>
+        <span>${payload.message}</span>
+        </li>
+        `;
+        const li = document.createElement('li');
+        li.innerHTML = newMessage;
+        messagesUl.append(li); 
+
+    // console.log(payload)        
+})
+        
 }
 
